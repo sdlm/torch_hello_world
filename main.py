61e736c0dab45993c1efbcacbdb44b9d35b367cd
sdlm <sdlmerr@yandex.ru>
2019-08-07 08:35:09 +0300

    Add Convolutional model

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ FIRST_HIDDEN_LAYER_SIZE = 128
 SECOND_HIDDEN_LAYER_SIZE = 64
 OUTPUT_SIZE = 10
 
+DEFAULT_EPOCHS = 15
+
 
 def get_data_loaders():
     train_set = torchvision.datasets.MNIST(
@@ -56,21 +58,51 @@ class FeedForward(torch.nn.Module):
         self.softmax = torch.nn.LogSoftmax(dim=1)
 
     def forward(self, x):
-        x1 = F.relu(self.linear1(x))  # .clamp(min=0)
+        x0 = x.reshape(x.size(0), -1)  # flat
+        x1 = F.relu(self.linear1(x0))  # .clamp(min=0)
         x2 = F.relu(self.linear2(x1))  # .clamp(min=0)
         x3 = self.linear3(x2)
         return self.softmax(x3)
 
 
-def train_model(train_loader, model, loss_f):
+class ConvNet(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3),  # 26 * 26
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)  # 13 * 13
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3),  # 11 * 11
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)  # 5 * 5
+        )
+        self.drop_out = nn.Dropout(p=.5)
+        self.fc1 = nn.Linear(5 * 5 * 64, 1000)  # Fully Connected
+        self.fc2 = nn.Linear(1000, 10)
+        self.softmax = torch.nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)  # flat
+        out = self.drop_out(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return self.softmax(out)
+
+
+def train_model(train_loader, model, loss_f, epochs):
     optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
     time0 = time()
-    epochs = 15
+    # epochs = 15
     for e in range(epochs):
         running_loss = 0
         for images, labels in train_loader:
             # Flatten MNIST images into a 784 long vector
-            images = images.view(images.shape[0], -1)
+            # images = images.view(images.shape[0], -1)
             if cuda.is_available():
                 images = images.cuda()
                 labels = labels.cuda()
@@ -101,15 +133,22 @@ def calculate_accuracy(val_loader, model):
     for images, labels in val_loader:
         if cuda.is_available():
             labels = labels.cuda()
+            images = images.cuda()
+
+        with torch.no_grad():
+            output = model(images)
+
         for i in range(len(labels)):
-            img = images[i].view(1, 784)
-            if cuda.is_available():
-                img = img.cuda()
-            with torch.no_grad():
-                logps = model(img)
+            # img = images[i].view(1, 784)
+            # img = images[i]
+            # if cuda.is_available():
+            #     img = img.cuda()
+            # with torch.no_grad():
+            #     logps = model(img)
+            logps = output[i]
 
             ps = torch.exp(logps)
-            probab = list(ps.cpu().numpy()[0])
+            probab = list(ps.cpu().numpy())
             pred_label = probab.index(max(probab))
             true_label = labels.cpu().numpy()[i]
             if true_label == pred_label:
@@ -124,9 +163,12 @@ if __name__ == '__main__':
 
     train_loader, val_loader = get_data_loaders()
 
-    model = FeedForward(
-        input_size=INPUT_SIZE, hidden_sizes=[FIRST_HIDDEN_LAYER_SIZE, SECOND_HIDDEN_LAYER_SIZE], output_size=OUTPUT_SIZE
-    )
+    model = ConvNet()
+    # model = FeedForward(
+    #     input_size=INPUT_SIZE,
+    #     hidden_sizes=[FIRST_HIDDEN_LAYER_SIZE, SECOND_HIDDEN_LAYER_SIZE],
+    #     output_size=OUTPUT_SIZE
+    # )
     # model = get_model()
     if cuda.is_available():
         model = model.cuda()
@@ -134,7 +176,7 @@ if __name__ == '__main__':
     loss_f = nn.NLLLoss()
     # loss_f = nn.CrossEntropyLoss()
 
-    train_model(train_loader, model, loss_f)
+    train_model(train_loader, model, loss_f, epochs=10)  # DEFAULT_EPOCHS
     torch.save(model, './data/my_mnist_model.pt')
 
     calculate_accuracy(val_loader, model)
